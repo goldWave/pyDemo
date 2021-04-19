f5ff99d733519302e691aed101f4ab549f535251
jimbo <ren.jinbo@navercorp.com>
2021-04-19 14:24:01 +0800

    test

--- ini_common_method.py
+++ ini_common_method.py
@@ -1,12 +1,40 @@
 # coding = utf-8
 
 import os
+from time import ctime, sleep
 
 dir_all_file = "D:\\licecap\\all_file.txt"
+dir_i_file = "D:\\licecap\\i_file.txt"
 dir_checked = "C:\\Users\\Administrator\\source\\PRISMLiveStudio\\src\\prism"
 
-def findAllCheckFile() -> list:
+def findAllCheckFile_i() -> list:
 	""" 获取所有的 cpp h 之类的文件列表 """
+	_allFiles = list()
+	if os.path.exists(dir_i_file):
+		with open(dir_i_file, 'r', encoding='utf-8') as f:
+			allLines = f.readlines()
+			for x in allLines:
+				x = x.strip('\n')
+				_allFiles.append(x)
+		return _allFiles
+
+	index = 0
+	for root, lists, files in os.walk(dir_checked):
+		for file in files:
+			if os.path.splitext(file)[1] in ['.i']:
+				# print(root + "=====" + file)
+				_allFiles.append(root+"\\"+file)
+
+	with open(dir_i_file, 'w', encoding='utf-8') as fp:
+		fp.truncate()
+		for x in _allFiles:
+			fp.write(x)
+			fp.write('\n')
+
+	return _allFiles
+
+def findAllCheckFile() -> list:
+	""" 获取所有的 .i 之类的文件列表 """
 	_allFiles = list()
 	if os.path.exists(dir_all_file):
 		with open(dir_all_file, 'r', encoding='utf-8') as f:
@@ -31,7 +59,6 @@ def findAllCheckFile() -> list:
 
 	return _allFiles
 
-
 def getINIKeyValues(dir) -> list:
 	""" 获取所有的  ini 文件的key 列表"""
 	with open (dir, 'r', encoding='UTF-8') as f:
@@ -46,7 +73,7 @@ def getINIKeyValues(dir) -> list:
 		return _list
 
 
-def openAndCheckUsedData(fileList, keyList, isMacro=False) -> list:
+def openAndCheckUsedData(fileList, keyList, isMacro=False, printProgress=False) -> list:
 	"""
 	fileList = 所有文件的路径
 	keyList = 查找的key
@@ -56,20 +83,30 @@ def openAndCheckUsedData(fileList, keyList, isMacro=False) -> list:
 
 	allCount = len(fileList)
 	# _notUsed = keyList
+	index = 1
+
 	for file in fileList:
-		# print(file)
+		if printProgress == True:
+			print(str(index) + "/" + str(allCount) + file)
+			index = index + 1
+		print('DONE AT--start:', ctime())
 		with open(file, 'r', encoding='utf-8') as f:
+			print('DONE AT--open:', ctime())
 			allLine = f.readlines()
-			strLine = ",".join(allLine)
+			# strLine = ",".join(allLine)
+			print('DONE AT--join:', ctime())
 			for i in range(len(keyList) - 1, -1, -1):
 				_key = keyList[i]
 				if isMacro:
 					if _key in strLine:
 						keyList.remove(_key)
-
 				else:
+					if ('" "' in strLine):
+						strLine = strLine.replace('" "', '');
 					if ('"' + _key + '"' in strLine) or ('<string>'+_key+'</string>' in strLine) or ('\"'+_key+'\"' in strLine):
 						keyList.remove(_key)
+
+		print('DONE AT--over:', ctime())
 
 	return keyList.copy()
 
--- pyTest.py
+++ pyTest.py
@@ -908,9 +908,75 @@
 # translator = Translator()
 # print(translator.translate('星期日').text)
 
-import sort
+# import sort
 
-input_num = [50, 123, 543, 187, 49, 30, 0, 2, 11, 100]
-# print("原始数据： " , input_num)
-a = sort.bubble_sort(input_num)
-print(a)
+# input_num = [50, 123, 543, 187, 49, 30, 0, 2, 11, 100]
+# # print("原始数据： " , input_num)
+# a = sort.bubble_sort(input_num)
+# print(a)
+
+
+# a = '	return QString::fromUtf8(Str("Basic.Settings.Output.Simple.Encoder." "Software"));'
+
+# if ('" "' in a):
+# 	a = a.replace('" "', '');
+
+# print(a)
+
+# -*-coding:utf-8-*-
+from time import ctime, sleep
+import threading
+# import numpy as np
+import collections
+ 
+loops = ['广州', '北京']
+t_list = ['01', '02', '03']
+cldas_sum = collections.deque()
+ 
+ 
+class MyThread(threading.Thread):
+    def __init__(self, func, args, name=''):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.func = func
+        self.args = args
+        self.result = self.func(*self.args)
+ 
+    def get_result(self):
+        try:
+            return self.result
+        except Exception:
+            return None
+ 
+ 
+def loop(nloop):
+    for j in t_list:
+        cldas_values = []
+        for k in range(4):
+            cldas_value = nloop + str(k)
+            cldas_values.append(cldas_value)
+        cldas_values.append(j)
+        cldas_values.append(nloop)
+        cldas_sum.append(cldas_values)
+        print(id(cldas_values))
+    #print(cldas_sum)
+    return cldas_sum
+ 
+ 
+def main():
+    print('start at', ctime())
+    threads = []
+    nloops = range(len(loops))
+    for i in nloops:
+        t = MyThread(loop, (loops[i],), loop.__name__)
+        threads.append(t)
+    for i in nloops:   # start threads 此处并不会执行线程，而是将任务分发到每个线程，同步线程。等同步完成后再开始执行start方法
+        threads[i].start()
+    for i in nloops:   # jion()方法等待线程完成
+        threads[i].join()
+    print(threads[1].get_result())
+    # print('DONE AT:', ctime())
+ 
+ 
+if __name__ == '__main__':
+    main()
